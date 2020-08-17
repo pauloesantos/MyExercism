dbe09141fc9508ae89069d79474234ae93761bea
Paulo <paulo.esantos@gmail.com>
2020-08-17 11:10:12 -0300

    Primeiro Commit

---
+++ csharp/hello-world/HelloWorldTest.cs
@@ -0,0 +1,17 @@
+// This file was auto-generated based on version 1.1.0 of the canonical data.
+
+using Xunit;
+
+public class HelloWorldTest
+{
+    [Fact]
+    public void Say_hi_()
+    {
+        Assert.Equal("Hello, World!", HelloWorld.Hello());
+    }
+    [Fact]
+    public void Not_Say_hi_()
+    {
+        Assert.NotEqual("Hello, World", HelloWorld.Hello());
+    }
+}
